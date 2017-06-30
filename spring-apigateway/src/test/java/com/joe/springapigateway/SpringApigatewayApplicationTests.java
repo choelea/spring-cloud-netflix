8cf38180a823d1ffae5df0eeff9d0a324ac43a75
Joe Li <joe.li@okchem.com>
2017-06-30 09:58:54 +0800

    Spring Cloud apigateway

---
+++ spring-apigateway/src/test/java/com/joe/springapigateway/SpringApigatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.joe.springapigateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringApigatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
