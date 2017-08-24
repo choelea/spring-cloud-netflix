d1c3de1b744e3cd979c7aaddc52877c192bd0533
Joe Li <joe.li@okchem.com>
2017-08-24 15:51:45 +0800

    Account Service

--- account-service/src/main/java/com/joe/accountservice/AccountServiceApplication.java
+++ account-service/src/main/java/com/joe/accountservice/AccountServiceApplication.java
@@ -2,11 +2,13 @@ package com.joe.accountservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
