dfa50983f63a20fabb8699cfc3b49b8d7aeb91fb
Joe Li <joe.li@okchem.com>
2017-08-24 15:50:20 +0800

    测试Config-Server

--- bookmark-service/src/main/java/com/joe/bookmark/BookmarkServiceApplication.java
+++ bookmark-service/src/main/java/com/joe/bookmark/BookmarkServiceApplication.java
@@ -2,16 +2,18 @@ package com.joe.bookmark;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.joe.bookmark.entity.Bookmark;
 import com.joe.bookmark.repository.BookmarkRepository;
-
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -32,6 +34,19 @@ public class BookmarkServiceApplication {
                             "A description for " + n + "'s link",
                             n)));
         };
+    }
+    
+    @RefreshScope
+    @RestController
+    class MessageRestController {
+
+        @Value("${message:Hello default}")
+        private String message;
+
+        @RequestMapping("/message")
+        String getMessage() {
+            return this.message;
+        }
     }
 }
 
